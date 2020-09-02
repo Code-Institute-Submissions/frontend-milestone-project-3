7a05dae79addeb8a07bcc2bafd3f225303d29d41
phillrutherford <phillip.h.rutherford@gmail.com>
2020-09-02 20:20:05 +0000

    code validated

--- assets/js/asia.js
+++ assets/js/asia.js
@@ -1,44 +1,42 @@
-/*-- Google Maps API for Asia --*/
+/* -- Google Maps API for Asia --*/
 
 function initMap() {
-            var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 4.5,
-                center: {
-                    lat: 13.75335,
-                    lng: 100.50483
-                }
-            });
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4.5,
+    center: {
+      lat: 13.75335,
+      lng: 100.50483,
+    },
+  });
 
-            var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+  const labels = 'ABCDEFGHIJKLMONPQRSTUVWXYZ';
 
-            var locations = [{
-                lat: 13.75335,
-                lng: 100.50483
-            }, {
-                lat: 18.767,
-                lng: 99.02616
-            }, {
-                lat: 21.02889,
-                lng: 105.85464
-            }, {
-                lat: 15.874985,
-                lng: 108.335976,
-            }, {
-                lat: 10.77824,
-                lng: 106.70324,
-            }, {
-                lat: 7.973667,
-                lng: 98.34393,
-            }];
+  const locations = [{
+    lat: 13.75335,
+    lng: 100.50483,
+  }, {
+    lat: 18.767,
+    lng: 99.02616,
+  }, {
+    lat: 21.02889,
+    lng: 105.85464,
+  }, {
+    lat: 15.874985,
+    lng: 108.335976,
+  }, {
+    lat: 10.77824,
+    lng: 106.70324,
+  }, {
+    lat: 7.973667,
+    lng: 98.34393,
+  }];
 
-            var markers = locations.map(function(location, i) {
-                return new google.maps.Marker({
-                    position: location,
-                    label: labels[i % labels.length]
-                });
-            });
+  const markers = locations.map((location, i) => new google.maps.Marker({
+    position: location,
+    label: labels[i % labels.length],
+  }));
 
-            var markerCluster = new MarkerClusterer(map, markers, {
-                imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-            });
-        }
+  const markerCluster = new MarkerClusterer(map, markers, {
+    imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
+  });
+}
